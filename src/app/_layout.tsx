951bc22099b005155ec847cd90245311ef97559d
Jakub Jirak <jirak.jakub12@seznam.cz>
2026-03-14 23:15:24 +0100

    feat: pridani praveho protected do layoutu

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,30 +1,46 @@
 import { COLORS } from "@/constants/COLORS";
-import { ReanimatedTrueSheetProvider } from "@lodev09/react-native-true-sheet/reanimated";
+import type { AuthUser } from "@/contexts/AuthContext";
+import AuthContext from "@/contexts/AuthContext";
+import { loadUser } from "@/services/AuthService";
 import { Stack } from "expo-router";
+import { useEffect, useState } from "react";
 import { PaperProvider } from "react-native-paper";
 import "../../global.css";
 
 export default function RootLayout() {
-  const isAuth = false;
+  const [user, setUser] = useState<AuthUser | null>(null);
+
+  useEffect(() => {
+    async function checkAuth() {
+      try {
+        const user = await loadUser();
+        setUser(user);
+      } catch (error) {
+        console.error("Error loading user:", error);
+      }
+    }
+    checkAuth();
+  }, []);
+
   return (
-    <ReanimatedTrueSheetProvider>
+    <AuthContext.Provider value={{ user, setUser }}>
       <PaperProvider>
         <Stack
           screenOptions={{
             contentStyle: { backgroundColor: COLORS.primary },
           }}
         >
-          <Stack.Protected guard={isAuth}>
+          <Stack.Protected guard={!!user}>
             <Stack.Screen name="(auth)" options={{ headerShown: false }} />
           </Stack.Protected>
 
-          <Stack.Protected guard={!isAuth}>
+          <Stack.Protected guard={!user}>
             <Stack.Screen name="index" options={{ headerShown: false }} />
             <Stack.Screen name="login" options={{ headerShown: false }} />
             <Stack.Screen name="register" options={{ headerShown: false }} />
           </Stack.Protected>
         </Stack>
       </PaperProvider>
-    </ReanimatedTrueSheetProvider>
+    </AuthContext.Provider>
   );
 }
