88df763e0fb007215fe053f6856cfe0d2f4e40a7
Jakub Jirak <jirak.jakub12@seznam.cz>
2026-03-17 17:04:28 +0100

    fix: oddelani nepotrebne screen

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout() {
         }}
       >
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="proj" options={{ headerShown: false }} />
       </Stack>
     </SafeAreaView>
   );
