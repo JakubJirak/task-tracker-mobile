4a9b1d494f3a8d5f9ce5685c7fb8d3553295ee7f
Jakub Jirak <jirak.jakub12@seznam.cz>
2026-03-17 17:04:19 +0100

    feat: pridani theme provideru

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,6 +4,7 @@ import type { AuthUser } from "@/contexts/AuthContext";
 import AuthContext from "@/contexts/AuthContext";
 import { loadUser } from "@/services/AuthService";
 import axiosClient from "@/utils/axios";
+import { DarkTheme, ThemeProvider } from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Stack } from "expo-router";
 import { useEffect, useState } from "react";
@@ -29,29 +30,42 @@ export default function RootLayout() {
     axios: axiosClient,
   });
 
+  const MyTheme = {
+    ...DarkTheme,
+    colors: {
+      ...DarkTheme.colors,
+      background: COLORS.primary,
+    },
+  };
+
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <AuthContext.Provider value={{ user, setUser }}>
-        <PaperProvider>
-          <Stack
-            screenOptions={{
-              contentStyle: { backgroundColor: COLORS.primary },
-            }}
-          >
-            <Stack.Protected guard={!!user}>
-              <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-            </Stack.Protected>
+    <ThemeProvider value={MyTheme}>
+      <QueryClientProvider client={queryClient}>
+        <AuthContext.Provider value={{ user, setUser }}>
+          <PaperProvider>
+            <Stack
+              screenOptions={{
+                contentStyle: { backgroundColor: COLORS.primary },
+              }}
+            >
+              <Stack.Protected guard={!!user}>
+                <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+              </Stack.Protected>
 
-            <Stack.Protected guard={!user}>
-              <Stack.Screen name="index" options={{ headerShown: false }} />
-              <Stack.Screen name="login" options={{ headerShown: false }} />
-              <Stack.Screen name="register" options={{ headerShown: false }} />
-            </Stack.Protected>
-          </Stack>
-        </PaperProvider>
-      </AuthContext.Provider>
-    </QueryClientProvider>
+              <Stack.Protected guard={!user}>
+                <Stack.Screen name="index" options={{ headerShown: false }} />
+                <Stack.Screen name="login" options={{ headerShown: false }} />
+                <Stack.Screen
+                  name="register"
+                  options={{ headerShown: false }}
+                />
+              </Stack.Protected>
+            </Stack>
+          </PaperProvider>
+        </AuthContext.Provider>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 }
